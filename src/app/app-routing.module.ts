51efa0afb573ea44f34390500446d24aa926cc7a
namnd <dinhnambk96@gmail.com>
2021-05-23 17:54:39 +0700

    create structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,59 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleComponent } from './components/article/article.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'profile/:username',
+    component: ProfileComponent,
+  },
+  {
+    path: 'profile/:username/favorites',
+    component: ProfileComponent,
+  },
+  {
+    path: 'editor',
+    component: EditorComponent,
+  },
+  {
+    path: 'editor/:slug',
+    component: EditorComponent,
+  },
+  {
+    path: 'article/:slug',
+    component: ArticleComponent,
+  },
+  {
+    path: 'login',
+    component: SignInComponent,
+  },
+  {
+    path: 'register',
+    component: SignUpComponent,
+  },
+  {
+    path: 'setting',
+    component: SettingComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './components/article/article.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    EditorComponent,
+    HomeComponent,
+    ProfileComponent,
+    SettingComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
